e63e6e0f795f3f6e2b51413f613360f568bcd65b
Kieran Barker <kieran11397@gmail.com>
2019-10-14 09:29:00 +0100

    Allow the single password field to be toggled

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-(function() {
+(function(d) {
+
   "use strict";
-})();
+
+  var toggle = d.querySelector("#show-password");
+
+  function togglePassword() {
+    var password = d.querySelector("#password");
+    password.type = (this.checked) ? "text" : "password";
+  }
+
+  toggle.addEventListener("change", togglePassword);
+
+})(document);

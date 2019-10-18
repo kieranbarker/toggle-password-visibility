68a3b359d2fd002e37c293272a0f8b108d08b87c
Kieran Barker <kieran11397@gmail.com>
2019-10-18 07:30:37 +0100

    Get (somewhat repetitive) code working, at least...

--- js/app.js
+++ js/app.js
@@ -2,19 +2,30 @@
 
   "use strict";
 
-  var toggle = d.querySelector("#show-passwords");
-  var passwords = d.querySelectorAll("[type='password']");
-
+  var forms = document.querySelectorAll("form");
+  
   function togglePassword(field) {
-    field.type = (toggle.checked) ? "text" : "password";
+    field.type = (event.target.checked) ? "text" : "password";
   }
 
-  function toggleAllPasswords() {
+  function toggleAllPasswords(event) {
+    if (!event.target.hasAttribute("data-toggle")) return;
+    var passwords = event.target.closest("form").querySelectorAll("[data-password]");
     passwords.forEach(togglePassword);
   }
 
-  if (toggle.checked) toggleAllPasswords();
+  function togglePasswordsOnLoad(form) {
+    var toggle = form.querySelector("[data-toggle]");
+    var passwords = form.querySelectorAll("[data-password]");
+    passwords.forEach(function(password) {
+      password.type = (toggle.checked) ? "text" : "password";
+    });
+  }
 
-  toggle.addEventListener("change", toggleAllPasswords, false);
+  // Firefox persists checkbox state, so for each form,
+  // toggle its password(s) on page load if its checkbox is checked
+  forms.forEach(togglePasswordsOnLoad);
+
+  document.body.addEventListener("change", toggleAllPasswords, false);
 
 })(document);
